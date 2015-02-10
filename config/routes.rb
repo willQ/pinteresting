454ec5e9f5a75f290afcea3771500ba150610985
Will McQuain <will.mcquain@gmail.com>
2015-02-09 19:17:01 -0800

    matt adds changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   
-  root 'welcome#index' 
+  root 'pages#home' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
