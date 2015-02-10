8d6a0c5b650459e8d27f9d4efa0217f5ca6fd748
Will McQuain <will.mcquain@gmail.com>
2015-02-09 19:21:35 -0800

    fixing matts error

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  root 'pages#home;' 
+  root 'pages#home' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
